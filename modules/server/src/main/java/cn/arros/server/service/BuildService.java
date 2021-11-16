b27806defb156d112de3be70262abe4d0d234275
Verge <jinxuyang@163.com>
2021-11-16 23:11:52 +0800

    refactor(build_service): add some new feature
    1. refactor MavenUtils to MavenInvokerBuild
    2. complete BuildService

--- modules/server/src/main/java/cn/arros/server/service/BuildService.java
+++ modules/server/src/main/java/cn/arros/server/service/BuildService.java
@@ -1,20 +1,25 @@
 package cn.arros.server.service;
 
+import cn.arros.server.component.MavenInvokerBuilder;
 import cn.arros.server.constant.BuildStatus;
+import cn.arros.server.constant.DeployType;
 import cn.arros.server.entity.BuildHistory;
 import cn.arros.server.entity.BuildInfo;
 import cn.arros.server.mapper.BuildHistoryMapper;
 import cn.arros.server.utils.GitUtils;
-import cn.arros.server.utils.MavenUtils;
-import cn.hutool.core.util.IdUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Supplier;
 
 /**
@@ -23,14 +28,19 @@ import java.util.function.Supplier;
  * @Version 1.0
  */
 public class BuildService implements Runnable{
+    private final static Logger logger = LoggerFactory.getLogger(BuildService.class);
 
     private final BuildInfo buildInfo;
 
-    BuildHistory buildHistory = new BuildHistory();
+    private final BuildHistory buildHistory = new BuildHistory();
 
-    BuildHistoryMapper buildHistoryMapper = SpringUtil.getBean(BuildHistoryMapper.class);
+    private final BuildHistoryMapper buildHistoryMapper = SpringUtil.getBean(BuildHistoryMapper.class);
 
-    BuildHistoryService buildHistoryService = SpringUtil.getBean(BuildHistoryService.class);
+    private final BuildHistoryService buildHistoryService = SpringUtil.getBean(BuildHistoryService.class);
+
+    private final ThreadPoolExecutor threadPoolExecutor = SpringUtil.getBean(ThreadPoolExecutor.class);
+
+    private final MavenInvokerBuilder mavenInvokerBuilder = SpringUtil.getBean(MavenInvokerBuilder.class);
 
     public BuildService(BuildInfo buildInfo) {
         this.buildInfo = buildInfo;
@@ -65,7 +75,8 @@ public class BuildService implements Runnable{
      * @return 成功与否
      */
     private boolean prepare() {
-        //buildHistory.setId(IdUtil.fastSimpleUUID());
+        logger.info("进入准备阶段");
+
         buildHistory.setStartTime(LocalDateTime.now());
         buildHistory.setBuildInfoId(buildInfo.getId());
 
@@ -79,6 +90,7 @@ public class BuildService implements Runnable{
      * @return 成功与否
      */
     private boolean updateSource() {
+        logger.info("正在更新代码");
         buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.BUILDING);
 
         try {
@@ -96,21 +108,36 @@ public class BuildService implements Runnable{
      * @return 成功与否
      */
     private boolean build() {
-        boolean status = MavenUtils.pack(buildInfo.getRepoId());
-        if (status) {
-            buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.BUILD_COMPLETED);
-        } else {
+        logger.info("开始构建");
+
+        try {
+            mavenInvokerBuilder.build(buildInfo.getRepoId(), buildInfo.getBuildCommand());
+        } catch (MavenInvocationException e) {
+            e.printStackTrace();
+            logger.error("构建时出现错误:{}",e.getMessage());
             buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.BUILD_FAILED);
+            return false;
         }
-        return status;
+
+        buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.BUILD_COMPLETED);
+        logger.info("构建成功");
+        return true;
     }
 
-    // TODO: 待完成
 
     private boolean deploy(){
-        buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.DEPLOYING);
-        buildHistory.setEndTime(LocalDateTime.now());
-        buildHistoryMapper.updateById(buildHistory);
+        logger.info("开始部署");
+        // 部署
+        if (Objects.equals(buildInfo.getDeployType(), DeployType.DEPLOY_TO_HOST.getType())) {
+            threadPoolExecutor.execute(new DeployService(buildInfo, buildHistory));
+            buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.DEPLOYING);
+        } else {
+            // 不部署
+            buildHistory.setEndTime(LocalDateTime.now());
+            buildHistoryMapper.updateById(buildHistory);
+            buildHistoryService.updateBuildStatus(buildHistory.getId(), BuildStatus.BUILD_COMPLETED);
+        }
+
         return true;
     }
 
