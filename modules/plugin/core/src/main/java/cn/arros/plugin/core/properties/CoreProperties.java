3578cb882cdb89167ef9178f33b4c37f1f441af4
Verge <jinxuyang@163.com>
2021-12-06 20:43:13 +0800

    fix(core): optimize import

--- modules/plugin/core/src/main/java/cn/arros/plugin/core/properties/CoreProperties.java
+++ modules/plugin/core/src/main/java/cn/arros/plugin/core/properties/CoreProperties.java
@@ -1,9 +1,7 @@
 package cn.arros.plugin.core.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @Author Verge
