482379cb7b81622619e98722d841ebb36bf6fe95
Verge <jinxuyang@163.com>
2021-12-25 21:28:43 +0800

    feat(cli): complete cli framework

--- modules/arros-cli/src/main/java/ArrosCli.java
+++ modules/arros-cli/src/main/java/ArrosCli.java
@@ -1,11 +1,21 @@
-import org.jline.reader.EndOfFileException;
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.reader.UserInterruptException;
+import org.fusesource.jansi.AnsiConsole;
+import org.jline.console.SystemRegistry;
+import org.jline.console.impl.SystemRegistryImpl;
+import org.jline.keymap.KeyMap;
+import org.jline.reader.*;
+import org.jline.reader.impl.DefaultParser;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.widget.TailTipWidgets;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.shell.jline3.PicocliCommands;
+import picocli.shell.jline3.PicocliCommands.PicocliCommandsFactory;
 
-import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
 
 /**
  * @Author Verge
@@ -13,26 +23,77 @@ import java.io.IOException;
  * @Version 1.0
  */
 public class ArrosCli {
+    /**
+     * Top-level command that just prints help.
+     */
+    @Command(
+            description = {"Arros项目命令行界面"},
+            subcommands = {
+                    PicocliCommands.ClearScreen.class,
+                    CommandLine.HelpCommand.class
+    })
+    static class CliCommands implements Runnable {
+        PrintWriter out;
+
+        public void setReader(LineReader reader){
+            out = reader.getTerminal().writer();
+        }
+
+        public void run() {
+            out.println(new CommandLine(this).getUsageMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        try (Terminal terminal = TerminalBuilder.builder().build()) {
-            LineReader lineReader = LineReaderBuilder.builder()
-                    .terminal(terminal)
-                    .build();
-            String prompt = "arros> ";
-            while (true) {
+        AnsiConsole.systemInstall();
+        try {
+            Supplier<Path> workDir = () -> Paths.get(System.getProperty("user.dir"));
+
+            CliCommands commands = new CliCommands();
+            PicocliCommandsFactory factory = new PicocliCommandsFactory();
+            CommandLine cmd = new CommandLine(commands, factory);
+            PicocliCommands picocliCommands = new PicocliCommands(cmd);
+
+            Parser parser = new DefaultParser();
+            try (Terminal terminal = TerminalBuilder.builder().build()) {
+                SystemRegistry systemRegistry = new SystemRegistryImpl(parser, terminal, workDir, null);
+                systemRegistry.setCommandRegistries(picocliCommands);
+                systemRegistry.register("help", picocliCommands);
+
+                LineReader reader = LineReaderBuilder.builder()
+                        .terminal(terminal)
+                        .completer(systemRegistry.completer())
+                        .parser(parser)
+                        .variable(LineReader.LIST_MAX, 50)   // max tab completion candidates
+                        .build();
+                commands.setReader(reader);
+                factory.setTerminal(terminal);
+
+                TailTipWidgets widgets = new TailTipWidgets(reader, systemRegistry::commandDescription, 5, TailTipWidgets.TipType.COMPLETER);
+                widgets.enable();
+                KeyMap<Binding> keyMap = reader.getKeyMaps().get("main");
+                keyMap.bind(new Reference("tailtip-toggle"), KeyMap.alt("s"));
+
+                String prompt = "arros>";
+                String rightPrompt = null;
                 String line;
-                try {
-                    line = lineReader.readLine(prompt);
-                    System.out.println(line);
-                } catch (UserInterruptException e) {
-                    System.out.println(e);
-                } catch (EndOfFileException e) {
-                    System.out.println("\nBye.");
-                    return;
+                while (true) {
+                    try {
+                        systemRegistry.cleanUp();
+                        line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
+                        systemRegistry.execute(line);
+                    } catch (EndOfFileException e) {
+                        System.out.println("Bye!");
+                        return;
+                    } catch (Exception e) {
+                        systemRegistry.trace(e);
+                    }
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            AnsiConsole.systemUninstall();
         }
     }
 }
