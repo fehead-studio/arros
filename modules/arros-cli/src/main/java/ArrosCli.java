cb61f4a086b650ff6e4ef0652a0167052915b33d
Verge <jinxuyang@163.com>
2021-12-25 17:29:17 +0800

    feat(cli): add cli framework

--- modules/arros-cli/src/main/java/ArrosCli.java
+++ modules/arros-cli/src/main/java/ArrosCli.java
@@ -1,24 +1,38 @@
-import picocli.CommandLine;
-import picocli.CommandLine.*;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-import java.util.concurrent.Callable;
+import java.io.IOException;
 
 /**
  * @Author Verge
  * @Date 2021/12/24 19:50
  * @Version 1.0
  */
-@Command(name = "arroscli", mixinStandardHelpOptions = true, version = "1.0.0",
-        description = "Arros项目cli.")
-public class ArrosCli implements Callable<Integer>  {
-
-    @Override
-    public Integer call() throws Exception {
-        return null;
-    }
-
+public class ArrosCli {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new ArrosCli()).execute(args);
-        System.exit(exitCode);
+        try (Terminal terminal = TerminalBuilder.builder().build()) {
+            LineReader lineReader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+            String prompt = "arros> ";
+            while (true) {
+                String line;
+                try {
+                    line = lineReader.readLine(prompt);
+                    System.out.println(line);
+                } catch (UserInterruptException e) {
+                    System.out.println(e);
+                } catch (EndOfFileException e) {
+                    System.out.println("\nBye.");
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
